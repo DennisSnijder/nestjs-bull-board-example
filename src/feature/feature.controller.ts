14990eb4e6122897a2de10a298e28dda658031a5
Dennis Snijder <dennissnijder97@gmail.com>
2023-06-10 20:10:12 +0200

    Added the new InjectBullBoard decorator to the feature controller example

--- src/feature/feature.controller.ts
+++ src/feature/feature.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get, Inject } from "@nestjs/common";
-import { BULL_BOARD_INSTANCE, BullBoardInstance } from "nestjs-bull-board";
+import { Controller, Get } from "@nestjs/common";
+import { BullBoardInstance, InjectBullBoard } from "nestjs-bull-board";
 
 @Controller('my-feature')
 export class FeatureController {
 
-
   constructor(
-    // You can simply inject the bull-board instance using the @Inject decorator and the exported constant.
-    @Inject(BULL_BOARD_INSTANCE) private readonly boardInstance: BullBoardInstance
+    //inject the bull-board instance using the provided decorator
+    @InjectBullBoard() private readonly boardInstance: BullBoardInstance
   ) {
   }
 
